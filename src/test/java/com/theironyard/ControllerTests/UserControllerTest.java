d30aa33845ecef1a8c2558331574026bef1fbb61
Alex Hanger <ahanger@experientgroup.com>
2016-05-15 20:23:33 -0400

    set up tests for user service and controller

--- src/test/java/com/theironyard/ControllerTests/UserControllerTest.java
+++ src/test/java/com/theironyard/ControllerTests/UserControllerTest.java
@@ -1,7 +1,37 @@
 package com.theironyard.ControllerTests;
 
+import com.theironyard.TestApplication;
+import com.theironyard.users_core.rest.UserController;
+import com.theironyard.users_core.service.UserService;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
 /**
  * Created by ahanger on 5/15/2016.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {TestApplication.class})
+@WebAppConfiguration
 public class UserControllerTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserControllerTest.class);
+    private final String SUCCESS_MESSAGE = "Success";
+    private final String SUCCESS_CODE = "200";
+
+    protected MockMvc mockMvc;
+    protected UserController userController;
+    protected UserService userService;
+
+    @Autowired
+    public UserControllerTest(MockMvc mockMvc, UserController userController, UserService userService) {
+        this.mockMvc = mockMvc;
+        this.userController = userController;
+        this.userService = userService;
+    }
 }
--- src/test/java/com/theironyard/ServiceTests/UserServiceTest.java
+++ src/test/java/com/theironyard/ServiceTests/UserServiceTest.java
@@ -1,7 +1,33 @@
 package com.theironyard.ServiceTests;
 
+import com.theironyard.TestApplication;
+import com.theironyard.users_core.service.UserService;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
 /**
  * Created by ahanger on 5/15/2016.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {TestApplication.class})
+@WebAppConfiguration
 public class UserServiceTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserServiceTest.class);
+    private final String SUCCESS_MESSAGE = "Success";
+    private final String SUCCESS_CODE = "200";
+
+    private UserService userService;
+
+    @Autowired
+    public UserServiceTest(UserService userService) {
+        this.userService = userService;
+    }
+
+
 }
